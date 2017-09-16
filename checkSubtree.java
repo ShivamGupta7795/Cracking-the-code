ec8a7020c88198a7b2298920f3185cac87a4041e
sgupta33 <sgupta33>
2017-09-16 19:50:56 -0400

    check if a tree is a subtree of another tree

---
+++ checkSubtree.java
@@ -0,0 +1,32 @@
+
+public class checkSubtree {
+	public static void main(String[] ar){
+		
+	}
+	/*T1 is a million node tree. We have to find whether there exist a subtree in T1 which matches 
+	 * the structure of T2. Size of T2 is some hundreds of nodes.*/
+	public static boolean checksubtree(Btree T1, Btree T2){
+		if(T2==null){
+			return true;
+		}
+		if(T1.data==T2.data){
+			if(matchsubtree(T1,T2)){
+				return true;
+			}
+		}
+		return checksubtree(T1.left,T2) || checksubtree(T1.right,T2);
+	}
+	
+	private static boolean matchsubtree(Btree T1, Btree T2){
+		if(T2==null && T2 == null){
+			return true;
+		}
+		if(T2==null || T2 == null){
+			return false;
+		}
+		if(T2.data!=T2.data){
+			return false;
+		}
+		return matchsubtree(T1.left, T2.left) && matchsubtree(T1.right, T2.right);
+	}
+}
