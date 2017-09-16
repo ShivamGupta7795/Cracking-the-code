0e45d7fed1deae7363a0c5baadd27e28da67f99b
sgupta33 <sgupta33>
2017-09-16 19:49:12 -0400

    Check if a binary tree is a binary search tree

--- CheckBST.java
+++ CheckBST.java
@@ -9,6 +9,9 @@ public class CheckBST {
 	public static boolean checkBST(Btree root){
 		return checkBST(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
 	}
+	public static boolean checkBSTWithInorder(Btree root){
+		return checkBSTInorder(root);
+	}
 	private static boolean checkBST(Btree root, int min, int max){
 		if(root==null){
 			return true;
@@ -18,4 +21,25 @@ public class CheckBST {
 		}
 		return checkBST(root.left, min, root.data-1)&&checkBST(root.right, root.data+1, max);
 	}
+	/*Method 2: Use inorder traversal*/
+	private static boolean checkBSTInorder(Btree root){
+		int lastelement  = Integer.MIN_VALUE;
+		if(root ==null){
+			return true;
+		}
+		if(!checkBSTInorder(root.left)){
+			return false;
+		}
+		if(root.data<lastelement){
+			return false;
+		}
+		lastelement = root.data;
+		
+		if(!checkBSTInorder(root.right)){
+			return false;
+		}
+		return true;
+	}
+	
+	
 }
