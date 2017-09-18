9074e638d3924bfd9a2e7b4fd0346d1403f40b3c
sgupta33 <sgupta33>
2017-09-18 03:06:08 -0400

    Find longest non repititive substring

--- longestSubstring.java
+++ longestSubstring.java
@@ -1,26 +1,26 @@
 package leetcode;
 import java.util.HashMap;
-/*Problem:
- * Given a string, find the length of the longest substring without repeating characters.
-Examples:
-Given "aabcbaa", the answer is "cba", with the length of 3. Note that the answer must be a substring, not a subsequence.
- * Approach:
-Add the characters and their position to the map. If a character repeats then the length of the new string becomes
-maximum of the (current length of the substring) or (current pointer - (index of repetitive character))+1. Max will store maximum of 
-length of such substrings.*/
+
 public class longestSubstring{
-	public int lengthOfLongestSubstring(String s) {
-	       int length = 0, max= 0;
-	        char[] charArray = s.toCharArray();
-	        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
-	        for(int i=0;i<charArray.length;i++){
-	            if(map.containsKey(charArray[i])){
-	                length = Math.max(length, map.get(charArray[i])+1);
-	            }
-	            
-	            map.put(charArray[i],i);
-	            max = Math.max(max,i-length+1);
-	        }
-	        return max;
-	    }
+	/*Problem:
+	 * Given a string, find the length of the longest substring without repeating characters.
+	Examples:
+	Given "aabcbada", the answer is "cbad", with the length of 4. Note that the answer must be a substring, not a subsequence.
+	 * Approach:
+    Add the characters and their position to the map. If a character repeats then the length of the new string becomes
+    maximum of current length of substring or (current pointer - (index of repetitive character))+1. Max will store maximum of 
+    length of such substrings.*/
+    public int lengthOfLongestSubstring(String s) {
+       int length = 0, max= 0;
+        char[] charArray = s.toCharArray();
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+        for(int i=0;i<charArray.length;i++){
+            if(map.containsKey(charArray[i])){
+                length = Math.max(length, map.get(charArray[i])+1);
+            }
+            map.put(charArray[i],i);
+            max = Math.max(max,i-length+1);
+        }
+        return max;
+    }
 }
