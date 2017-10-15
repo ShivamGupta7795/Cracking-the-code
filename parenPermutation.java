fc22c29ad3d3739e500a92ed129afcbe59c88703
sgupta33 <sgupta33>
2017-10-14 20:10:35 -0400

    find valid combinations of n pairs of parentheses

--- parenPermutation.java
+++ parenPermutation.java
@@ -7,7 +7,13 @@ public class parenPermutation {
 		ArrayList<String> paren = new parenPermutation().findPermutation(4);
 		int count = 0;
 		for(int i=0;i<paren.size();i++){
-			System.out.println(++count+": "+paren.get(i));
+			System.out.println(++count+":paren: "+paren.get(i));
+		}
+		
+		ArrayList<String> perm= new parenPermutation().generatePerm(4);
+		count = 0;
+		for(int i=0;i<perm.size();i++){
+			System.out.println(++count+": "+perm.get(i));
 		}
 		
 	}
@@ -29,6 +35,31 @@ public class parenPermutation {
 			return result;
 		}
 		return paren;
+	}
+	
+	private void findPermutation(ArrayList<String> list, int leftrem, int rightrem, char[] str, int count){
+		if(leftrem<0 || rightrem<leftrem) return;
+		
+		if(leftrem==0 && rightrem==0){
+			String s = String.copyValueOf(str);
+			list.add(s);
+		}else{
+			if(leftrem>0){
+				str[count] = '(';
+				findPermutation(list, leftrem-1, rightrem, str, count+1);
+			}
+			if(rightrem>leftrem){
+				str[count] = ')';
+				findPermutation(list, leftrem, rightrem-1, str, count+1);
+			}
+		}
+	}
+	
+	public ArrayList<String> generatePerm(int count){
+		char[] str= new char[count*2];
+		ArrayList<String> list= new ArrayList<String>();
+		findPermutation(list, count, count, str, 0);
+		return list;
 	}
 	
 }
