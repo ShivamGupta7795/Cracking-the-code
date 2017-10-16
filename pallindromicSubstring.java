8fd1f55684010743c60d51f64a75edeffa619b72
sgupta33 <sgupta33>
2017-10-15 21:46:41 -0400

    find the longest pallindromic substring

--- pallindromicSubstring.java
+++ pallindromicSubstring.java
@@ -1,5 +1,15 @@
 package leetcode;
+/*
+ * Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
 
+Example:
+
+Input: "babad"
+
+Output: "bab"
+
+Note: "aba" is also a valid answer.
+ * */
 public class pallindromicSubstring {
 	int max, index;
     public String longestPalindrome(String s) {
