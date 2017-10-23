62eef71ef3273d71c6294eced1801fe0770230c3
Shivam Gupta <shivam.gupta7795@gmail.com>
2017-10-23 17:42:01 -0400

    Search element in sorted array using Binary Search

--- BinarySearch.java
+++ BinarySearch.java
@@ -14,7 +14,7 @@ public class BinarySearch {
 		if(array[mid]==key){
 			return true;
 		}
-		if(array[mid]>key){
+		if(array[mid]<key){
 			return binarysearch(array, key, mid+1, right);
 		}else{
 			return binarysearch(array, key, left, mid-1);
