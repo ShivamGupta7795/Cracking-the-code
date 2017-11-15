652e165ed69f7a1ca02a2bd8139bd4d5362cd033
Shivam Gupta <shivam.gupta7795@gmail.com>
2017-11-14 19:43:08 -0500

    Coin change 1 and 2

--- coinChange.java
+++ coinChange.java
@@ -49,4 +49,19 @@ public class coinChange {
 	    }
 	    return DP[amount];
 	}
+	
+	
+	/*minimum number of coins needed to reach a target*/
+	public int coinChange(int[] coins, int amount) {
+		int[] dp = new int[amount+1];
+		Arrays.fill(dp, Integer.MAX_VALUE-1);
+		dp[0] = 0;
+		for(int coin:coins){
+		    for(int i=coin;i<=amount;i++){
+			if(dp[i-coin]+1<dp[i])
+			    dp[i] = dp[i-coin]+1;
+		    }
+		}
+		return (dp[amount]==Integer.MAX_VALUE-1)?-1:dp[amount];
+	}
 }
