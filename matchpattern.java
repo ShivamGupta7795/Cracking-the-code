b40c2178b7314757fec401e42fb7cf91f1eb0710
Shivam Gupta <shivam.gupta7795@gmail.com>
2017-11-19 22:10:48 -0500

    match word pattern

--- matchpattern.java
+++ matchpattern.java
@@ -1,5 +1,19 @@
 package leetcode;
 import java.util.*;
+/*
+Given a pattern and a string str, find if str follows the same pattern.
+
+Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty
+word in str.
+
+Examples:
+pattern = "abba", str = "dog cat cat dog" should return true.
+pattern = "abba", str = "dog cat cat fish" should return false.
+pattern = "aaaa", str = "dog cat cat dog" should return false.
+pattern = "abba", str = "dog dog dog dog" should return false.
+
+*/
+
 public class matchpattern {
     public boolean wordPattern(String pattern, String str) {
         /*Approach:
